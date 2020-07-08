5f5ed0f6262b0dd4c30967a545ed8386102e8825
Ly Van Chan <chan.ly@boot.ai>
2020-07-08 14:53:15 +0700

    update: update

--- core/calc.py
+++ core/calc.py
@@ -2,3 +2,37 @@ def add(x, y):
     return x + y
 
 
+def test_add():
+    assert add(5, 10) == 15
+    assert add(-1, 1) == 0
+
+
+def sum(x, y):
+    return x + y
+
+
+def test_sum():
+    assert sum(2, 3) == 5  # Should be 5
+    assert sum(1, 1) == 5  # Should be 2
+
+
+if __name__ == "__main__":
+    test_sum()
+
+import unittest
+
+
+def sum(x, y):
+    return x + y
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum(2, 3), 5)
+        self.assertNotEqual(sum(1, 1), 5)
+
+
+if __name__ == "__main__":
+    MyTestCase()
+
--- test_calc.py
+++ test_calc.py
@@ -29,3 +29,15 @@ def test_xfail():
 @pytest.mark.parametrize('num1, num2, result', [(7, 3, 10), (-1, 1, 0), (-1, 100, 99)])
 def test_add2(num1, num2, result):
     assert calc.add(num1, num2) == result
+
+
+def add_test():
+    assert calc.add(5, 10) == 15
+    assert calc.add(-1, 1) == 0
+
+
+def test_add_2():
+    assert calc.add(5, 10) == 15
+    assert calc.add(-1, 1) == 0
+
+
